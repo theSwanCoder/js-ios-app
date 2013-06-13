1ef54052f74d908894bab224f77b57376982f1da
Vlad Zavadskii <vzavadskii@jaspersoft.com>
2013-06-13 21:11:43 +0000

    Importing application with new architecture

---
+++ JasperMobileTests/JasperMobileTests.h
@@ -0,0 +1,13 @@
+//
+//  JasperMobileTests.h
+//  JasperMobileTests
+//
+//  Created by Vlad on 5/22/13.
+//  Copyright (c) 2013 com.jaspersoft. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JasperMobileTests : SenTestCase
+
+@end
---
+++ JasperMobileTests/JasperMobileTests.m
@@ -0,0 +1,32 @@
+//
+//  JasperMobileTests.m
+//  JasperMobileTests
+//
+//  Created by Vlad on 5/22/13.
+//  Copyright (c) 2013 com.jaspersoft. All rights reserved.
+//
+
+#import "JasperMobileTests.h"
+
+@implementation JasperMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JasperMobileTests");
+}
+
+@end
