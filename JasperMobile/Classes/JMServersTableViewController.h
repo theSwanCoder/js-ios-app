dabfe983d0e719c4a11d8ee1f725ab44ee766fe9
Vlad Zavadskii <vzavadskii@jaspersoft.com>
2013-07-08 12:37:10 +0300

    Add Core Data

---
+++ JasperMobile/Classes/JMServersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMServersTableViewController.h
+//  JasperMobile
+//
+//  Created by Vlad Zavadskii on 7/8/13.
+//  Copyright (c) 2013 com.jaspersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMServersTableViewController : UITableViewController
+
+@end
