ef447c657ffc03d755452c27bcc5d6e33f59cc83
Vlad Zavadskii <vzavadskii@jaspersoft.com>
2013-07-31 23:17:32 +0300

    Change start date in copyright to proper

--- JasperMobile/Classes/JMLibraryTableViewController.h
+++ JasperMobile/Classes/JMLibraryTableViewController.h
@@ -1,6 +1,6 @@
 /*
  * JasperMobile for iOS
- * Copyright (C) 2005 - 2013 Jaspersoft Corporation. All rights reserved.
+ * Copyright (C) 2011 - 2013 Jaspersoft Corporation. All rights reserved.
  * http://community.jaspersoft.com/project/jaspermobile-ios
  *
  * Unless you have purchased a commercial license agreement from Jaspersoft,
