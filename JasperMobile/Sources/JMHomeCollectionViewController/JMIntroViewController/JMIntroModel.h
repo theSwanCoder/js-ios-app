e13eea51e07e6b8267e79a059346a0f7538ab423
Aleksandr Dakhno <aleksandr.dakhno@gmail.com>
2014-11-08 19:49:50 +0200

    new image assets.

---
+++ JasperMobile/Sources/JMHomeCollectionViewController/JMIntroViewController/JMIntroModel.h
@@ -0,0 +1,10 @@
+//
+// Created by Aleksandr Dakhno on 11/8/14.
+// Copyright (c) 2014 Tibco JasperMobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface JMIntroModel : NSObject
+@end
---
+++ JasperMobile/Sources/JMHomeCollectionViewController/JMIntroViewController/JMIntroModel.m
@@ -0,0 +1,12 @@
+//
+// Created by Aleksandr Dakhno on 11/8/14.
+// Copyright (c) 2014 Tibco JasperMobile. All rights reserved.
+//
+
+#import "JMIntroModel.h"
+
+
+@implementation JMIntroModel {
+
+}
+@end
