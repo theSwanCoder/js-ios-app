62b8119f115d6024bd16f03c207cb43c060e5b79
Aleksandr Dakhno <aleksandr.dakhno@gmail.com>
2014-11-08 09:50:47 +0200

    created images assest

---
+++ JasperMobile/Sources/JMHomeCollectionViewController/JMIntroViewController/JMIntroChildViewController/JMIntroChildViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMIntrolChildViewController.h
+//  TIBCO JasperMobile
+//
+//  Created by Aleksandr Dakhno on 11/7/14.
+//  Copyright (c) 2014 Tibco JasperMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMIntrolChildViewController : UIViewController
+
+@end
---
+++ JasperMobile/Sources/JMHomeCollectionViewController/JMIntroViewController/JMIntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMIntroViewController.h
+//  TIBCO JasperMobile
+//
+//  Created by Aleksandr Dakhno on 11/6/14.
+//  Copyright (c) 2014 Tibco JasperMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMIntroViewController : UIViewController
+
+@end
