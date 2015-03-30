cc9aceeda020fdd9d97c60866a36713af195a373
Olexandr Dahno <odahno@tibco.com>
2015-03-30 13:21:17 +0300

    add test server profile (dashboard with visualize)

---
+++ JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMBaseResourceViewerVC/JMBaseResourceViewerVC.m
@@ -0,0 +1,13 @@
+//
+//  JMBaseResourceViewerVC.m
+//  TIBCO JasperMobile
+//
+//  Created by Aleksandr Dakhno on 3/30/15.
+//  Copyright (c) 2015 TIBCO JasperMobile. All rights reserved.
+//
+
+#import "JMBaseResourceViewerVC.h"
+
+@implementation JMBaseResourceViewerVC
+
+@end
