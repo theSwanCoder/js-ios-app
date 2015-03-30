8c02fd62ee09428c4450f24ea72345e261a63457
Olexandr Dahno <odahno@tibco.com>
2015-03-30 16:51:16 +0300

    extract separate class for run dashboard

--- JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMDashboardsViewerViewController/JMDashboardVC.swift
+++ JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMDashboardsViewerViewController/JMDashboardVC.swift
@@ -36,11 +36,15 @@ import WebKit
 class JMDashboardVC: JMResourceViewerVC {
 
     var dashboard: JMDashboard?
+    var dashboardLoader: JMVisualizeDashboardLoader?
 
     deinit {
-        destroyDashboard()
+        if let loader = dashboardLoader {
+            loader.destroy()
+        }
     }
 
+    // overrided functions
     override func currentResourceLookup() -> JSResourceLookup! {
         var resourceLookup : JSResourceLookup?
         if let dashboard = self.dashboard {
@@ -49,95 +53,18 @@ class JMDashboardVC: JMResourceViewerVC {
         return resourceLookup
     }
 
-    // Actions
-    func destroyDashboard() {
-        let destroyDashboardJS = "MobileDashboard.destroy();"
-        if let webView = self.webView {
-            webView.evaluateJavaScript(destroyDashboardJS, completionHandler: nil)
-        }
-    }
-
     // Start point
     override func runReportExecution() {
-        loadDashboard()
-    }
-
-    func loadDashboard() {
-        if let dashboard = self.dashboard {
-            if JMWKWebViewManager.sharedInstance.isVisualizeLoaded {
-                runDashboard()
-            } else {
-                if let htmlString = HTMLString() {
-                    let baseURLString = self.restClient().serverProfile.serverUrl
-                    if let webView = self.webView {
-                        JMWKWebViewManager.sharedInstance.isVisualizeLoaded = true
-                        webView.loadHTMLString(htmlString, baseURL: NSURL(string: baseURLString))
-                    }
-                }
-            }
-        }
-    }
-
-    // run dashboard
-    func runDashboard() {
-        let authorizeString = "MobileDashboard.authorize({'username': '\(self.restClient().serverProfile.username)', 'password': '\(self.restClient().serverProfile.password)', 'organization': '\(self.restClient().serverProfile.organization)'});"
-        if let webView = self.webView {
-            webView.evaluateJavaScript(authorizeString, completionHandler: nil)
-        }
 
         if let dashboard = self.dashboard {
-            let runDashboardString = "MobileDashboard.run({'uri': '\(dashboard.resourceURI)'});"
-            if let webView = self.webView {
-                webView.evaluateJavaScript(runDashboardString, completionHandler: nil)
+            dashboardLoader = JMVisualizeDashboardLoader(dashboard: dashboard)
+            if let loader = dashboardLoader {
+                loader.webView = webView
+                loader.webView?.navigationDelegate = loader
+                loader.run()
             }
+        } else {
+            println("dashboard isn't assigned")
         }
     }
-}
-
-extension JMDashboardVC {
-
-    // helpers
-    func HTMLString() -> String? {
-
-        let dashboardHTMLPath = NSBundle.mainBundle().pathForResource("dashboard", ofType: "html")
-        var dashboardHTMLContent = String(contentsOfFile: dashboardHTMLPath!, encoding: NSUTF8StringEncoding, error: nil)
-
-        var htmlStringWithVisualizePath: String?
-
-        if let htmlString = dashboardHTMLContent {
-            let baseURLString = self.restClient().serverProfile.serverUrl
-
-            htmlStringWithVisualizePath = htmlString.stringByReplacingOccurrencesOfString("VISUALIZE_PATH", withString: baseURLString + "/client/visualize.js", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-
-            // requreJS
-            let requireJSPath = NSBundle.mainBundle().pathForResource("require.min", ofType: "js")
-            let requireJSContent = String(contentsOfFile: requireJSPath!, encoding: NSUTF8StringEncoding, error: nil)
-            if let requireJS = requireJSContent {
-                htmlStringWithVisualizePath = htmlStringWithVisualizePath!.stringByReplacingOccurrencesOfString("REQUIRE_JS", withString: requireJS, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-            }
-
-            // mobilejs
-            let mobileJSPath = NSBundle.mainBundle().pathForResource("dashboard-amber2-ios-mobilejs-sdk", ofType: "js")
-            let mobileJSContent = String(contentsOfFile: mobileJSPath!, encoding: NSUTF8StringEncoding, error: nil)
-            if let mobileJS = mobileJSContent {
-                htmlStringWithVisualizePath = htmlStringWithVisualizePath!.stringByReplacingOccurrencesOfString("JASPERMOBILE_SCRIPT", withString: mobileJS, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-            }
-        }
-        return htmlStringWithVisualizePath
-    }
-}
-
-extension JMDashboardVC: WKNavigationDelegate {
-
-    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
-
-        let isLinkClicked = navigationAction.navigationType == .LinkActivated
-        let requestString = navigationAction.request.URL.absoluteString!
-
-        println("request: \(requestString)")
-        println("isLinkClicked: \(isLinkClicked)")
-        decisionHandler(.Allow)
-
-    }
-
 }
--- JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMDashboardsViewerViewController/JMVisualizeDashboardLoader/JMVisualizeDashboardLoader.swift
+++ JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMDashboardsViewerViewController/JMVisualizeDashboardLoader/JMVisualizeDashboardLoader.swift
@@ -1,13 +1,141 @@
+/*
+* TIBCO JasperMobile for iOS
+* Copyright © 2005-2014 TIBCO Software, Inc. All rights reserved.
+* http://community.jaspersoft.com/project/jaspermobile-ios
+*
+* Unless you have purchased a commercial license agreement from Jaspersoft,
+* the following license terms apply:
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/lgpl>.
+*/
+
+
 //
 //  JMVisualizeDashboardLoader.swift
 //  TIBCO JasperMobile
 //
-//  Created by Aleksandr Dakhno on 3/30/15.
-//  Copyright (c) 2015 TIBCO JasperMobile. All rights reserved.
-//
+
+/**
+@author Aleksandr Dakhno odahno@tibco.com
+@since 2.1
+*/
 
 import UIKit
+import WebKit
 
 class JMVisualizeDashboardLoader: NSObject {
-   
+
+    weak var webView: WKWebView?
+    let dashboard: JMDashboard
+
+    init(dashboard: JMDashboard) {
+        self.dashboard = dashboard
+    }
+
+    // public api
+    // start point
+    func run() {
+        loadDashboard()
+    }
+
+    func destroy() {
+        if let webView = self.webView {
+            let destroyDashboardJS = "MobileDashboard.destroy();"
+            webView.evaluateJavaScript(destroyDashboardJS, completionHandler: nil)
+        }
+    }
+
+
+    // private api
+    private func loadDashboard() {
+        if JMWKWebViewManager.sharedInstance.isVisualizeLoaded {
+            runDashboard()
+        } else {
+            if let htmlString = HTMLString() {
+                let baseURLString = self.restClient().serverProfile.serverUrl
+                if let webView = self.webView {
+                    JMWKWebViewManager.sharedInstance.isVisualizeLoaded = true
+                    webView.loadHTMLString(htmlString, baseURL: NSURL(string: baseURLString))
+                }
+            }
+        }
+    }
+
+    private func runDashboard() {
+        let authorizeString = "MobileDashboard.authorize({'username': '\(self.restClient().serverProfile.username)', 'password': '\(self.restClient().serverProfile.password)', 'organization': '\(self.restClient().serverProfile.organization)'});"
+        if let webView = self.webView {
+            webView.evaluateJavaScript(authorizeString, completionHandler: nil)
+        }
+
+        let runDashboardString = "MobileDashboard.run({'uri': '\(dashboard.resourceURI)'});"
+        if let webView = self.webView {
+            webView.evaluateJavaScript(runDashboardString, completionHandler: nil)
+        }
+    }
+}
+
+extension JMVisualizeDashboardLoader: WKNavigationDelegate {
+
+    func webView(webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        println("webView didStartProvisionalNavigation")
+    }
+
+    func webView(webView: WKWebView!, didFinishNavigation navigation: WKNavigation!) {
+        println("webView didFinishNavigation")
+    }
+
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+
+        let isLinkClicked = navigationAction.navigationType == .LinkActivated
+        let requestString = navigationAction.request.URL.absoluteString!
+
+        println("request: \(requestString)")
+        println("isLinkClicked: \(isLinkClicked)")
+        decisionHandler(.Allow)
+
+    }
+}
+
+extension JMVisualizeDashboardLoader {
+
+    // helpers
+    func HTMLString() -> String? {
+
+        let dashboardHTMLPath = NSBundle.mainBundle().pathForResource("dashboard", ofType: "html")
+        var dashboardHTMLContent = String(contentsOfFile: dashboardHTMLPath!, encoding: NSUTF8StringEncoding, error: nil)
+
+        var htmlStringWithVisualizePath: String?
+
+        if let htmlString = dashboardHTMLContent {
+            let baseURLString = self.restClient().serverProfile.serverUrl
+
+            htmlStringWithVisualizePath = htmlString.stringByReplacingOccurrencesOfString("VISUALIZE_PATH", withString: baseURLString + "/client/visualize.js", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+
+            // requreJS
+            let requireJSPath = NSBundle.mainBundle().pathForResource("require.min", ofType: "js")
+            let requireJSContent = String(contentsOfFile: requireJSPath!, encoding: NSUTF8StringEncoding, error: nil)
+            if let requireJS = requireJSContent {
+                htmlStringWithVisualizePath = htmlStringWithVisualizePath!.stringByReplacingOccurrencesOfString("REQUIRE_JS", withString: requireJS, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+            }
+
+            // mobilejs
+            let mobileJSPath = NSBundle.mainBundle().pathForResource("dashboard-amber2-ios-mobilejs-sdk", ofType: "js")
+            let mobileJSContent = String(contentsOfFile: mobileJSPath!, encoding: NSUTF8StringEncoding, error: nil)
+            if let mobileJS = mobileJSContent {
+                htmlStringWithVisualizePath = htmlStringWithVisualizePath!.stringByReplacingOccurrencesOfString("JASPERMOBILE_SCRIPT", withString: mobileJS, options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+            }
+        }
+        return htmlStringWithVisualizePath
+    }
 }
