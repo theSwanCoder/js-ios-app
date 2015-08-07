b7bc8dcc238c5a66312648333024064f1a125c6b
Alexey Gubarev <gubarev.lesha@gmail.com>
2015-08-07 19:59:22 +0300

    Fixed bug with displaying multy line title and error on input control cells.

--- JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMResourceViewerViewController/ReportViewer/Filter Options/InputControls/Cells/JMInputControlCell.m
+++ JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMResourceViewerViewController/ReportViewer/Filter Options/InputControls/Cells/JMInputControlCell.m
@@ -42,6 +42,22 @@
     self.errorLabel.textColor = [[JMThemesManager sharedManager] tableViewCellErrorColor];
 }
 
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    // Make sure the contentView does a layout pass here so that its subviews have their frames set, which we
+    // need to use to set the preferredMaxLayoutWidth below.
+    [self.contentView setNeedsLayout];
+    [self.contentView layoutIfNeeded];
+    
+    // Set the preferredMaxLayoutWidth of the mutli-line bodyLabel based on the evaluated width of the label's frame,
+    // as this will allow the text to wrap correctly, and as a result allow the label to take on the correct height.
+    self.titleLabel.preferredMaxLayoutWidth = CGRectGetWidth(self.titleLabel.frame);
+    self.errorLabel.preferredMaxLayoutWidth = CGRectGetWidth(self.errorLabel.frame);
+}
+
 - (void) updateDisplayingOfErrorMessage
 {
     NSString *errorString = [self.inputControlDescriptor errorString];
--- JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMResourceViewerViewController/ReportViewer/Filter Options/JMReportOptionsViewController.m
+++ JasperMobile/Sources/ViewControllers/Repository&Libraries/ResourceViewer/JMResourceViewerViewController/ReportViewer/Filter Options/JMReportOptionsViewController.m
@@ -59,6 +59,8 @@ NSInteger const kJMReportOptionsTableViewCellHeight = 44.f;
                                forState:UIControlStateNormal];
     [self.runReportButton setTitle:JMCustomLocalizedString(@"dialog.button.run.report", nil)
                           forState:UIControlStateNormal];
+    
+    self.tableView.estimatedRowHeight = UITableViewAutomaticDimension;
 }
 
 #pragma mark - Actions
@@ -151,19 +153,43 @@ NSInteger const kJMReportOptionsTableViewCellHeight = 44.f;
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     JSInputControlDescriptor *inputControlDescriptor = [self.inputControls objectAtIndex:indexPath.row];
-    if ([inputControlDescriptor errorString]) {
-        CGFloat maxWidth = tableView.frame.size.width - 2 * kJMReportOptionsTableViewCellHorizontalOffset;
-        if ([self isSelectableCellAtIndexPath:indexPath]) {
-            maxWidth -= 25;
-        }
-        CGSize maximumLabelSize = CGSizeMake(maxWidth, CGFLOAT_MAX);
-        CGRect textRect = [[inputControlDescriptor errorString] boundingRectWithSize:maximumLabelSize
-                                                                             options:(NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading)
-                                                                          attributes:@{NSFontAttributeName:[[JMThemesManager sharedManager] tableViewCellErrorFont]}
-                                                                             context:nil];
-        return kJMReportOptionsTableViewCellHeight + kJMReportOptionsTableViewCellVerticalOffset + ceil(textRect.size.height);
-    }
-    return kJMReportOptionsTableViewCellHeight;
+    NSString *cellIdentifier = [[self inputControlDescriptorTypes] objectForKey:inputControlDescriptor.type];
+ 
+    // This solution was taken form http://stackoverflow.com/a/18746930/2523825
+    
+    // Use the dictionary of offscreen cells to get a cell for the reuse identifier, creating a cell and storing
+    // it in the dictionary if one hasn't already been added for the reuse identifier.
+    // WARNING: Don't call the table view's dequeueReusableCellWithIdentifier: method here because this will result
+    // in a memory leak as the cell is created but never returned from the tableView:cellForRowAtIndexPath: method!
+    JMInputControlCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+
+    // Configure the cell for this indexPath
+    [cell setInputControlDescriptor:inputControlDescriptor];
+    
+    // The cell's width must be set to the same size it will end up at once it is in the table view.
+    // This is important so that we'll get the correct height for different table view widths, since our cell's
+    // height depends on its width due to the multi-line UILabel word wrapping. Don't need to do this above in
+    // -[tableView:cellForRowAtIndexPath:] because it happens automatically when the cell is used in the table view.
+    cell.bounds = CGRectMake(0.0f, 0.0f, CGRectGetWidth(tableView.bounds), CGRectGetHeight(cell.bounds));
+    // NOTE: if you are displaying a section index (e.g. alphabet along the right side of the table view), or
+    // if you are using a grouped table view style where cells have insets to the edges of the table view,
+    // you'll need to adjust the cell.bounds.size.width to be smaller than the full width of the table view we just
+    // set it to above. See http://stackoverflow.com/questions/3647242 for discussion on the section index width.
+    
+    // Do the layout pass on the cell, which will calculate the frames for all the views based on the constraints
+    // (Note that the preferredMaxLayoutWidth is set on multi-line UILabels inside the -[layoutSubviews] method
+    // in the UITableViewCell subclass
+    [cell setNeedsLayout];
+    [cell layoutIfNeeded];
+    
+    // Get the actual height required for the cell
+    CGFloat height = [cell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+    
+    // Add an extra point to the height to account for the cell separator, which is added between the bottom
+    // of the cell's contentView and the bottom of the table view cell.
+    height += 1;
+    
+    return height;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
