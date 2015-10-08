d3ba406b30373d72171de1077edf975974cc2725
Olexandr Dahno <aleksandr.dakhno@gmail.com>
2015-10-08 14:44:04 +0300

    setup viewport for 'old' dashboards.

---
+++ JasperMobile/Vendor/VisualizeJS/old_dashboard.js
@@ -0,0 +1,3 @@
+var metaTag=document.createElement('meta');
+metaTag.name = 'viewport'; metaTag.content = 'initial-scale=INITIAL_SCALE_VIEWPORT, width=device-width, minimum-scale=INITIAL_SCALE_VIEWPORT, maximum-scale=2';
+document.getElementsByTagName('head')[0].appendChild(metaTag);
