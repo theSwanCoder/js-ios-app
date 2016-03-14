43f4e4e6cbbaecbbd078ac61b459e4a17084880e
Olexandr Dahno <odahno@tibco.com>
2016-03-14 15:23:43 +0200

    refactor rest flow. move code related to handling report components to sdk.

--- JasperMobile/Vendor/VisualizeJS/vis_jaspermobile.js
+++ JasperMobile/Vendor/VisualizeJS/vis_jaspermobile.js
@@ -151,17 +151,87 @@ JasperMobile.Report.REST.API = {
             "isReady" : document.getElementById("container") != null
         });
     },
-    renderChart: function(script, params, renderFullScreen) {
-        if (renderFullScreen) {
-            var containerWidth = document.getElementById("container").offsetWidth;
-            var containerHeight = document.getElementById("container").offsetHeight;
+    loadScript: function(scriptPath) {
+        var isScriptAlreadyLoaded = false;
+        var allScripts = document.head.getElementsByTagName("script");
 
-            var chartDimensions = params["chartDimensions"];
-            chartDimensions["width"] = containerWidth;
-            chartDimensions["height"] = containerHeight;
-            params["chartDimensions"] = chartDimensions;
+        for(var i=0; i < allScripts.length; i++) {
+            var script = allScripts[i];
+            if (script.src === scriptPath) {
+                isScriptAlreadyLoaded = true;
+                JasperMobile.Callback.Callbacks.successCompleted("JasperMobile.Report.REST.API.loadScript", {
+                    "params" : {
+                        "script_path" : scriptPath
+                    }
+                });
+                break;
+            }
         }
+        if (!isScriptAlreadyLoaded) {
+            var scriptTag = document.createElement('script');
+            scriptTag.src = scriptPath;
+            scriptTag.onload = function() {
+                JasperMobile.Callback.Callbacks.successCompleted("JasperMobile.Report.REST.API.loadScript", {
+                    "params" : {
+                        "script_path" : scriptPath
+                    }
+                });
+            };
+            document.head.appendChild(scriptTag);
+        }
+    },
+    renderAdHocHighchart: function(script, params) {
+        var containerWidth = document.getElementById("container").offsetWidth;
+        var containerHeight = document.getElementById("container").offsetHeight;
+
+        var chartDimensions = params["chartDimensions"];
+        chartDimensions["width"] = containerWidth;
+        chartDimensions["height"] = containerHeight;
+        params["chartDimensions"] = chartDimensions;
         script(params);
+        JasperMobile.Callback.Callbacks.successCompleted("JasperMobile.Report.REST.API.renderAdHocHighchart", {});
+    },
+    renderHighcharts: function(script, paramsArray) {
+        for(var i=0; i < paramsArray.length; i++) {
+            var params = paramsArray[i];
+            script(params);
+        }
+        JasperMobile.Callback.Callbacks.successCompleted("JasperMobile.Report.REST.API.renderHighcharts", {});
+    },
+    renderFusionWidgets: function(paramsArray, jrsDomain) {
+        for(var i=0; i < paramsArray.length; i++) {
+            var params = paramsArray[i];
+            params["swfUrl"] = jrsDomain + params["swfUrl"];
+            JasperMobile.Report.REST.API.renderFusionWidget(params);
+        }
+        JasperMobile.Callback.Callbacks.successCompleted("JasperMobile.Report.REST.API.renderFusionWidgets", {});
+    },
+    renderFusionWidget: function(params) {
+        FusionCharts.options.html5ChartsSrc = "../charts/FusionCharts.HC.Charts.js";
+        FusionCharts.options.html5WidgetsSrc = "../widgets/FusionCharts.HC.Widgets.js";
+
+        var chartVar = window.fc_Fusion_841726305 = new FusionCharts({
+            "id"                : params["id"],
+            "renderer"          : "javascript", // TODO: base on input params
+            "swfUrl"            : params["swfUrl"],
+            "width"             : params["width"],
+            "height"            : params["height"],
+            "debugMode"         : params["debugMode"],
+            "registerWithJS"    : params["registerWithJS"],
+            "renderAt"          : params["renderAt"],
+            "allowScriptAccess" : params["allowScriptAccess"],
+            "dataFormat"        : params["dataFormat"],
+            "dataSource"        : "<chart showOpenValue='0' showHighLowValue='0' showCloseValue='0' lineColor='58595b' lineThickness='1' openColor='004682' closeColor='004682' anchorColor='0980ba' highColor='004682' lowColor='004682' drawAnchors='1' anchorRadius='2' bgColor='dfe0e2' exportEnabled='1' exportHandler='http://mobiledemo2.jaspersoft.com/jasperserver-pro/FCExporter' exportAtClient='0' exportAction='download' exportFileName='chart'><dataset><set value='217'/><set value='156'/><set value='197'/><set value='176'/><set value='109'/></dataset></chart>"
+        });
+
+        chartVar.addEventListener('BeforeRender', function(event, eventArgs) {
+            if (eventArgs.renderer === 'javascript') {
+                event.sender.setChartAttribute('exportEnabled', '0');
+            }
+        });
+
+        chartVar.setTransparent(params["transparent"]);
+        chartVar.render();
     },
     addHyperlinks: function(hyperlinks) {
         var allSpans = document.getElementsByTagName("span");
@@ -848,7 +918,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     JasperMobile.Callback.onScriptLoaded();
     JasperMobile.Helper.updateViewPortScale(0.3);
     JasperMobile.Helper.updateDocumentZoom(2);
-    //document.body.bgColor = "#f3f3f3";
+    document.body.bgColor = "#f3f3f3";
 });
 
 window.onerror = function myErrorHandler(message, source, lineno, colno, error) {
