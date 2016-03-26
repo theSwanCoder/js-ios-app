90a2669323012483033aac92c14aeabc154dc6bd
Olexandr Dahno <odahno@tibco.com>
2016-03-26 16:03:05 +0200

    create a new category for schedule lookup.

---
+++ JasperMobile/Sources/Categories/Resources/Schedules/JSScheduleLookup+Helpers.m
@@ -0,0 +1,10 @@
+//
+// Created by Aleksandr Dakhno on 3/26/16.
+// Copyright (c) 2016 TIBCO JasperMobile. All rights reserved.
+//
+
+#import "JSScheduleLookup+Helpers.h"
+
+
+@implementation JSScheduleLookup (Helpers)
+@end
