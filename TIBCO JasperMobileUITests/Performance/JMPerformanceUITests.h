6c26836b03dec2d3f6851baa6baef88dbec79539
Olexandr Dahno <odahno@tibco.com>
2016-05-17 07:47:45 +0300

    First performance test for CI (run second report).

---
+++ TIBCO JasperMobileUITests/Performance/JMPerformanceUITests.h
@@ -0,0 +1,13 @@
+//
+//  JMPerformanceUITests.h
+//  TIBCO JasperMobile
+//
+//  Created by Aleksandr Dakhno on 5/16/16.
+//  Copyright © 2016 TIBCO JasperMobile. All rights reserved.
+//
+
+#import "JMBaseUITestCase.h"
+
+@interface JMPerformanceUITests : JMBaseUITestCase
+
+@end
