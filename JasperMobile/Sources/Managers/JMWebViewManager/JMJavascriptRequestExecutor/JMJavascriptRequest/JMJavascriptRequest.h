545274ec3b5516520b8595ab633e4a1fcfedb761
Olexandr Dahno <odahno@tibco.com>
2016-07-24 20:36:17 +0300

    add another state in JMJavascriptRequestError enumeration - JMJavascriptRequestErrorSessionDidRestore.

--- JasperMobile/Sources/Managers/JMWebViewManager/JMJavascriptRequestExecutor/JMJavascriptRequest/JMJavascriptRequest.h
+++ JasperMobile/Sources/Managers/JMWebViewManager/JMJavascriptRequestExecutor/JMJavascriptRequest/JMJavascriptRequest.h
@@ -34,6 +34,7 @@
 typedef NS_ENUM(NSInteger, JMJavascriptRequestErrorType) {
     JMJavascriptRequestErrorTypeWindow,
     JMJavascriptRequestErrorTypeAuth,
+    JMJavascriptRequestErrorSessionDidRestore,
     JMJavascriptRequestErrorTypeUnexpected,
     JMJavascriptRequestErrorTypeOther,
 };
