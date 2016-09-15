c8eec610a4dd5f3d480dc4a7324d6cdff8ed44a9
Olexandr Dahno <odahno@tibco.com>
2016-09-15 08:55:47 +0300

    Update an url for test profile (use https protocol).

--- TIBCO JasperMobileUITests/Helpers/JMUITestConstants/JMUITestConstants.m
+++ TIBCO JasperMobileUITests/Helpers/JMUITestConstants/JMUITestConstants.m
@@ -11,7 +11,7 @@
 NSString *const kJMTestLibrarySearchTextExample = @"sales mix";
 // Test Profile Strings
 NSString *const kJMTestProfileName = @"Test Profile";
-NSString *const kJMTestProfileURL = @"http://mobiledemo.jaspersoft.com/jasperserver-pro";
+NSString *const kJMTestProfileURL = @"https://mobiledemo.jaspersoft.com/jasperserver-pro";
 NSString *const kJMTestProfileCredentialsUsername = @"phoneuser";
 NSString *const kJMTestProfileCredentialsOrganization = @"organization_1";
 NSString *const kJMTestProfileCredentialsPassword = @"phoneuser";
