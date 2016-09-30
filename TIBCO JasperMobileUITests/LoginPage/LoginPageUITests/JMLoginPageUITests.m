458abf7455eae21e1421b40b93bbc9fae5acd15c
Olexandr Dahno <odahno@tibco.com>
2016-09-30 09:55:34 +0300

    Implement test cases for JMLoginPageUITests.

--- TIBCO JasperMobileUITests/LoginPage/LoginPageUITests/JMLoginPageUITests.m
+++ TIBCO JasperMobileUITests/LoginPage/LoginPageUITests/JMLoginPageUITests.m
@@ -7,14 +7,389 @@
 //
 
 #import "JMLoginPageUITests.h"
+#import "JMBaseUITestCase+Helpers.h"
+#import "JMBaseUITestCase+SideMenu.h"
 
 @implementation JMLoginPageUITests
 
 #pragma mark - Tests
-- (void)testThatAlwaysFail
+
+- (void)setUp
 {
-//    XCTFail(@"Not implemented tests");
+    [super setUp];
 }
 
+#pragma mark - JMBaseUITestProtocol
+- (BOOL) shouldLoginBeforeStartTest
+{
+    return NO;
+}
+
+#pragma mark - Tests
+
+//- Launch screen
+//    - Precondition:
+//      - Launch Application ->
+//    - Result:
+//      - User should see Launch screen
+//      - Copyright should be correct
+- (void)testThatUserCanSeeLaunchScreen
+{
+    [self verifyThatLaunchPageOnScreen];
+}
+
+//- Log In screen
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Result:
+//      - User should see Login screen
+- (void)testThatUserCanSeeLoginScreen
+{
+    [self verifyThatLoginPageOnScreen];
+}
+
+//- TIBCO JasperMobile logo
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Result:
+//      - User should see TIBCO JasperMobile logo
+- (void)testThatUserCanSeeCorrectTrademarksOnLoginScreen
+{
+    [self verifyThatLoginPageHasCorrectTrademarks];
+}
+
+//- Try Demo Button
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Steps:
+//      - Tap ‘Try Demo’ button
+//    - Result:
+//      - User should connect to Mobile Demo Server
+- (void)testThatUserCanSeeLoginUsingTryDemoButton
+{
+    [self tapTryDemoButton];
+    [self waitLoginProcessDidFinish];
+
+    [self verifyThatUserDidLoginIntoDemoServer];
+}
+
+//- Try login with empty username
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Steps:
+//      - Make ‘username’ field empty
+//      - Enter right password into ‘password’ field
+//      - Select test server
+//      - Tap ‘Login’ button
+//    - Result:
+//      - User should see error message “Error. Specify a valid username’
+- (void)testThatUserCanSeeErrorWhenTryLoginWithEmptyUsername
+{
+    [self enterUsername:@""];
+    [self enterPassword:kJMTestProfileCredentialsPassword];
+    [self selectTestProfile];
+    [self tapLoginButton];
+
+    [self verifyThatErrorAlertOnScreenWithTitle:@"Error" message:@"Specify a valid username."];
+    
+    [self closeErrorAlertWithTitle:@"Error"];
+}
+
+//- Try login if username includes only spaces
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Steps:
+//      - Enter spaces into ‘username’ field
+//      - Enter right password into ‘password’ field
+//      - Select test server
+//      - Tap ‘Login’ button
+//    - Result:
+//      - User should see error message “Invalid credentials supplied. Could not login to JasperReports Server”
+- (void)testThatUserCanSeeErrorWhenTryLoginWithSpacesInUsername
+{
+    [self enterUsername:@"  "];
+    [self enterPassword:kJMTestProfileCredentialsPassword];
+    [self selectTestProfile];
+    [self tapLoginButton];
+    
+    [self verifyThatErrorAlertOnScreenWithTitle:@"Invalid credentials supplied." message:@"Could not login to JasperReports Server."];
+    
+    [self closeErrorAlertWithTitle:@"Invalid credentials supplied."];
+}
+
+//- Try login if username is incorrect
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Steps:
+//      - Enter wrong username into ‘username’ field
+//      - Enter right password into ‘password’ field
+//      - Select test server
+//      - Tap ‘Login’ button
+//    - Result:
+//      - User should see error message “Invalid credentials supplied. Could not login to JasperReports Server”
+- (void)testThatUserCanSeeErrorWhenTryLoginWithWrongUsername
+{
+    [self enterUsername:@"Wrong username"];
+    [self enterPassword:kJMTestProfileCredentialsPassword];
+    [self selectTestProfile];
+    [self tapLoginButton];
+    
+    [self verifyThatErrorAlertOnScreenWithTitle:@"Invalid credentials supplied." message:@"Could not login to JasperReports Server."];
+    
+    [self closeErrorAlertWithTitle:@"Invalid credentials supplied."];
+}
+
+//- Try login with empty password
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Steps:
+//      - Enter right username into ‘username’ field
+//      - Make ‘password’ field empty
+//      - Select test server
+//      - Tap ‘Login’ button
+//    - Result:
+//      - User should see error message “Error. Specify a valid password”
+- (void)testThatUserCanSeeErrorWhenTryLoginWithEmptyPassword
+{
+    [self enterUsername:kJMTestProfileCredentialsUsername];
+    [self enterPassword:@""];
+    [self selectTestProfile];
+    [self tapLoginButton];
+    
+    [self verifyThatErrorAlertOnScreenWithTitle:@"Error" message:@"Specify a valid password."];
+    
+    [self closeErrorAlertWithTitle:@"Error"];
+}
+
+//- Try login if password includes only spaces
+//    - Precondition:
+//      - Launch Application ->
+//      - Wait launch screen disappears ->
+//    - Steps:
+//      - Enter right username into ‘username’ field
+//      - Enter spaces into ‘password’ field
+//      - Select test server
+//      - Tap ‘Login’ button
+//    - Result:
+//      - User should see error message “Invalid credentials supplied. Could not login to JasperReports Server”
+- (void)testThatUserCanSeeErrorWhenTryLoginWithSpacesInPassword
+{
+    [self enterUsername:kJMTestProfileCredentialsUsername];
+    [self enterPassword:@"  "];
+    [self selectTestProfile];
+    [self tapLoginButton];
+
+    [self verifyThatErrorAlertOnScreenWithTitle:@"Invalid credentials supplied." message:@"Could not login to JasperReports Server."];
+    
+    [self closeErrorAlertWithTitle:@"Invalid credentials supplied."];
+}
+
+//- Try login if password is incorrect
+//    - Precondition:
+//    - Launch Application ->
+//    - Wait launch screen disappears ->
+//    - Steps:
+//    - Enter right username into ‘username’ field
+//    - Enter wrong password into ‘password’ field
+//    - Select test server
+//    - Tap ‘Login’ button
+//    - Result:
+//    - User should see error message “Invalid credentials supplied. Could not login to JasperReports Server”
+- (void)testThatUserCanSeeErrorWhenTryLoginWithWrongPassword
+{
+    [self enterUsername:kJMTestProfileCredentialsUsername];
+    [self enterPassword:@"Wrong password"];
+    [self selectTestProfile];
+    [self tapLoginButton];
+    
+    [self verifyThatErrorAlertOnScreenWithTitle:@"Invalid credentials supplied." message:@"Could not login to JasperReports Server."];
+    
+    [self closeErrorAlertWithTitle:@"Invalid credentials supplied."];
+}
+
+//- Try login with empty server
+//    - Precondition:
+//    - Launch Application ->
+//    - Wait launch screen disappears ->
+//    - Steps:
+//    - Enter right username into ‘username’ field
+//    - Enter right password into ‘password’ field
+//    - Don’t Select any server
+//    - Tap ‘Login’ button
+//    - Result:
+//    - User should see error message “Error. Select a server profile”
+- (void)testThatUserCanSeeErrorWhenTryLoginWithoutSelectedServer
+{
+    [self enterUsername:kJMTestProfileCredentialsUsername];
+    [self enterPassword:kJMTestProfileCredentialsPassword];
+    [self tapLoginButton];
+    
+    [self verifyThatErrorAlertOnScreenWithTitle:@"Error" message:@"Select a server profile."];
+    
+    [self closeErrorAlertWithTitle:@"Error"];
+}
+
+//- Try login with valid credentials
+//    - Precondition:
+//    - Launch Application ->
+//    - Wait launch screen disappears ->
+//    - Steps:
+//    - Enter right username into ‘username’ field
+//    - Enter right password into ‘password’ field
+//    - Select test server
+//    - Tap ‘Login’ button
+//    - Result:
+//    - User should see Library screen
+- (void)testThatUserCanLoginToTestServer
+{
+    [self enterUsername:kJMTestProfileCredentialsUsername];
+    [self enterPassword:kJMTestProfileCredentialsPassword];
+    [self selectTestProfile];
+    [self tapLoginButton];
+
+    [self waitLoginProcessDidFinish];
+    [self verifyThatUserDidLoginIntoTestServer];
+}
+
+#pragma mark - Verifying
+
+- (void)verifyThatLaunchPageOnScreen
+{
+    // Skip for now because in this point we can see only login page (launch page was gone).
+}
+
+- (void)verifyThatLoginPageOnScreen
+{
+    [self waitElementWithAccessibilityId:@"JMLoginPageAccessibilityId"
+                                 timeout:kUITestsBaseTimeout];
+}
+
+- (void)verifyThatLoginPageHasCorrectTrademarks
+{
+    XCUIElement *logoImage = [self findImageWithAccessibilityId:@"im_full_name"];
+    if (!logoImage) {
+        XCTFail(@"There isn't an trademark element on Login Page");
+    }
+}
+
+- (void)verifyThatUserDidLoginIntoDemoServer
+{
+    [self showSideMenu];
+    [self verifyAccountWithUsername:@"phoneuser"
+                       organization:@"organization_1"
+                        profileName:@"Jaspersoft Mobile Demo"];
+    [self hideSideMenu];
+}
+
+- (void)verifyThatErrorAlertOnScreenWithTitle:(NSString *)title message:(NSString *)message
+{
+    XCUIElement *alert = [self waitAlertWithTitle:title
+                                          timeout:kUITestsBaseTimeout];
+    XCUIElement *errorMessageElement = [self findStaticTextWithText:message
+                                                      parentElement:alert];
+    if (!errorMessageElement) {
+        XCTFail(@"Wrong message on '%@' alert", title);
+    }
+}
+
+- (void)verifyThatUserDidLoginIntoTestServer
+{
+    [self showSideMenu];
+    [self verifyAccountWithUsername:kJMTestProfileCredentialsUsername
+                       organization:kJMTestProfileCredentialsOrganization
+                        profileName:kJMTestProfileName];
+    [self hideSideMenu];
+}
+
+- (void)verifyAccountWithUsername:(NSString *)username
+                     organization:(NSString *)organization
+                      profileName:(NSString *)profileName
+{
+    XCUIElement *sideMenuElement = [self waitElementWithAccessibilityId:@"JMSideApplicationMenuAccessibilityId"
+                                                                timeout:kUITestsBaseTimeout];
+    XCUIElement *usernameStaticText = [self findStaticTextWithText:username
+                                                     parentElement:sideMenuElement];
+    if (!usernameStaticText) {
+        XCTFail(@"Error of verifying 'Demo' account: username - is wrong");
+    }
+    if (organization) {
+        XCUIElement *organizationStaticText = [self findStaticTextWithText:organization
+                                                             parentElement:sideMenuElement];
+        if (!organizationStaticText) {
+            XCTFail(@"Error of verifying 'Demo' account: organization - is wrong");
+        }
+    }
+    XCUIElement *serverProfileStaticText = [self findStaticTextWithText:profileName
+                                                          parentElement:sideMenuElement];
+    if (!serverProfileStaticText) {
+        XCTFail(@"Error of verifying 'Demo' account: server profile - is wrong");
+    }
+}
+
+#pragma mark - Helpers
+
+- (void)tapTryDemoButton
+{
+    XCUIElement *loginPageElement = [self findElementWithAccessibilityId:@"JMLoginPageAccessibilityId"];
+    XCUIElement *demoButton = [self findButtonWithAccessibilityId:@"JMLoginPageTryButtonAccessibilityId"
+                                                    parentElement:loginPageElement];
+    if (demoButton) {
+        [demoButton tap];
+    } else {
+        XCTFail(@"There isn't an 'Try Demo' button on Login Page");
+    }
+}
+
+- (void)tapLoginButton
+{
+    XCUIElement *loginPageElement = [self findElementWithAccessibilityId:@"JMLoginPageAccessibilityId"];
+    XCUIElement *loginButton = [self findButtonWithAccessibilityId:@"JMLoginPageLoginButtonAccessibilityId"
+                                                    parentElement:loginPageElement];
+    if (loginButton) {
+        [loginButton tap];
+    } else {
+        XCTFail(@"There isn't an 'Login' button on Login Page");
+    }
+}
+
+- (void)waitLoginProcessDidFinish
+{
+    [self givenLoadingPopupNotVisible];
+}
+
+- (void)enterUsername:(NSString *)username
+{
+    XCUIElement *loginPageElement = [self findElementWithAccessibilityId:@"JMLoginPageAccessibilityId"];
+    [self enterText:username intoTextFieldWithAccessibilityId:@"JMLoginPageUserNameTextFieldAccessibilityId"
+      parentElement:loginPageElement
+      isSecureField:false];
+}
+
+- (void)enterPassword:(NSString *)password
+{
+    XCUIElement *loginPageElement = [self findElementWithAccessibilityId:@"JMLoginPageAccessibilityId"];
+    [self enterText:password intoTextFieldWithAccessibilityId:@"JMLoginPagePasswordTextFieldAccessibilityId"
+      parentElement:loginPageElement
+      isSecureField:true];
+}
+
+    - (void)closeErrorAlertWithTitle:(NSString *)title
+{
+    XCUIElement *alert = [self findAlertWithTitle:title];
+    XCUIElement *okButton = [self findButtonWithTitle:@"OK"
+                                        parentElement:alert];
+    if (okButton) {
+        [okButton tap];
+    } else {
+        XCTFail(@"Error of finding 'OK' button");
+    }
+}
 
 @end
